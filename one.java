db0186bf687ec61d66ba3fd2e9d3fb3390d0c382
Samsh <Samsh@github.com>
2018-02-01 14:07:11 -0500

    first commit

---
+++ one.java
@@ -0,0 +1,21 @@
+package human;
+import java.util.Scanner;
+public class one {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter Name");
+		String f=scanner.nextLine();
+		String mi=scanner.nextLine();
+		String l=scanner.nextLine();
+	
+		System.out.println("Career");
+		 String c=scanner.nextLine();
+		 
+		System.out.println("Age");
+		int a= scanner.nextInt();
+		
+		
+	}
+
+}
